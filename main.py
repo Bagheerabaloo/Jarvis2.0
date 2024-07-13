1fb5b5cf33eb21e19cde4a3c63ea710c503d88e9
Bagheerabaloo <valeriostefanelli@hotmail.it>
2024-07-13 19:16:44 +0200

    heroku bug on __init__.py files

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import sys
+import os
+
 from quotes import QuotesManager
+
+# __ add src directory to PYTHONPATH __
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
 quotes_manager = QuotesManager()
 quotes_manager.start()
