711e03ebd772a67197977e8945eced5269880b62
Bagheerabaloo <valeriostefanelli@hotmail.it>
2025-11-14 18:44:34 +0100

    add env_path for raspberry

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from dotenv import load_dotenv
+from pathlib import Path
 import sys
 import os
 
 # __ add src directory to PYTHONPATH __
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+
+env_path = Path(__file__).resolve().parent / ".env"
+load_dotenv(dotenv_path=env_path)
+
 
 from quotes import QuotesManager
 
